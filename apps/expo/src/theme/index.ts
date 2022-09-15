a0ac9ad31ffd21090c24b9cc21f46f35b3713606
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2022-09-15 16:48:22 +0200

    added theme

---
+++ apps/expo/src/theme/index.ts
@@ -0,0 +1,37 @@
+//colors, spacing, etc for expo app in typescript
+
+import { Dimensions } from 'react-native';
+
+export const COLORS = {
+  // base colors
+  primary: '#290197',
+  secondary: '#004BCF',
+  tertiary: '#0078ED',
+  dark: '#1E1F20',
+  light: '#fafafa',
+  darkGray: '#898C95',
+  lightGray: '#C5CCD6',
+}
+
+export const FONT_SIZES = {
+  text: 16,
+  title: 20,
+  subtitle: 18,
+  header: 24,
+  large: 32,
+}
+
+export const FONT_WEIGHTS = {
+  light: '200',
+  regular: '400',
+  medium: '500',
+  bold: '700',
+}
+
+export const SPACING = {
+  tiny: 4,
+  small: 8,
+  medium: 16,
+  large: 24,
+  huge: 32,
+}
