11fbbc6678627fe51148bc6e54da309aad5aa65d
berke <bab26@st-andrews.ac.uk>
2022-09-15 20:18:58 +0300

    removed acme/auth from expo dependencies

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -1,23 +1,19 @@
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import React, { ComponentType } from "react";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TRPCProvider } from "./utils/trpc";
-import { Session, SessionProvider } from "@acme/auth/client";
 
 import { HomeScreen } from "./screens/home";
-import { InitialProps } from "expo/build/launch/withExpoRoot.types";
 
-const App: ComponentType<InitialProps> = ({ session, ...props }) => {
+const App = () => {
   return (
-    <SessionProvider session={session}>
-      <TRPCProvider>
-        <SafeAreaProvider>
-          <HomeScreen />
-          <StatusBar />
-        </SafeAreaProvider>
-      </TRPCProvider>
-    </SessionProvider>
+    <TRPCProvider>
+      <SafeAreaProvider>
+        <HomeScreen />
+        <StatusBar />
+      </SafeAreaProvider>
+    </TRPCProvider>
   );
 };
 
