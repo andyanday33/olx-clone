9510e56cd922f59abee144a01a794e179cbc13ac
berke <bab26@st-andrews.ac.uk>
2022-09-15 18:37:04 +0300

    wrapped expo app inside a session provider

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -1,19 +1,23 @@
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { ComponentType } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TRPCProvider } from "./utils/trpc";
+import { Session, SessionProvider } from "@acme/auth/client";
 
 import { HomeScreen } from "./screens/home";
+import { InitialProps } from "expo/build/launch/withExpoRoot.types";
 
-const App = () => {
+const App: ComponentType<InitialProps> = ({ session, ...props }) => {
   return (
-    <TRPCProvider>
-      <SafeAreaProvider>
-        <HomeScreen />
-        <StatusBar />
-      </SafeAreaProvider>
-    </TRPCProvider>
+    <SessionProvider session={session}>
+      <TRPCProvider>
+        <SafeAreaProvider>
+          <HomeScreen />
+          <StatusBar />
+        </SafeAreaProvider>
+      </TRPCProvider>
+    </SessionProvider>
   );
 };
 
--- packages/auth/client/index.ts
+++ packages/auth/client/index.ts
@@ -1,2 +1,3 @@
 export { SessionProvider } from "next-auth/react";
 export { useSession, signIn, signOut } from "next-auth/react";
+export type { Session } from "next-auth";
